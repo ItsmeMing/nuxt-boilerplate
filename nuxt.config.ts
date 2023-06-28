7b066cb3338b04cea5a366cb94805ba65138413d
ngocminh <ngocminh@beeknights.com>
2023-06-29 04:38:07 +0700

    adjust api service utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  spaLoadingTemplate: false,
   app: {
     head: {
       title: 'Sixth Element',
--- utils/api-service.ts
+++ utils/api-service.ts
@@ -1,43 +1,57 @@
 //library's imports
 import { AxiosResponse } from 'axios'
 
-export async function APIService<T extends any>(
+export async function APIService<T>(
   repo: () => Promise<AxiosResponse<T>>,
   options: {
-    loading: 'table' | 'fullscreen' | 'none'
-    custom_success_message?: string
-    action?: {
-      method: CallableFunction
-      mode: 'extend' | 'override'
+    loading: '.sixth-table' | 'fullscreen' | 'none' | string
+    success?: {
+      custom_success_message?: string
+      callback?: (data: Awaited<Promise<T>>) => void
+      override_default?: boolean
+    }
+    fail?: {
+      custom_fail_message?: string
+      callback?: () => void
+      override_default?: boolean
     }
   }
 ): Promise<T> {
-  // const loadingStore = useLoadingStore()
+  let fullscreenLoading
   try {
-    const { loading, custom_success_message, action } = options
-    // if (loading === 'table') loadingStore.setTableLoading(true)
-    // if (loading === 'fullscreen') loadingStore.setfullscreenLoading(true)
+    const { loading, success } = options
+    if (loading !== 'none') {
+      fullscreenLoading = ElLoading.service({
+        lock: true,
+        fullscreen: loading === 'fullscreen' ? true : false,
+        target: loading === 'fullscreen' ? document.body : loading,
+        background: 'rgba(0, 0, 0, 0.7)'
+      })
+    }
     const result = await repo()
     const data = result.data as any //cheat
-    if (result.config.method === 'get') return data.data
-    if (!action || action?.mode === 'extend')
+    if (result.config.method !== 'get' && (!success || !success?.override_default)) {
       ElNotification({
         title: 'Success',
-        message: custom_success_message || data.message,
+        message: success?.custom_success_message || data.message,
         type: 'success'
       })
-    action?.method(data.data)
-    return data as Awaited<Promise<T>>
+    }
+    if (success?.callback) success?.callback(data.data as Awaited<Promise<T>>)
+    return data.data as Awaited<Promise<T>>
   } catch (error: unknown) {
     const e = error as APIFailResponse
-    ElNotification({
-      title: 'Error',
-      message: e.first_message || e.message,
-      type: 'error'
-    })
+    const { fail } = options
+    if (!fail || !fail?.override_default) {
+      ElNotification({
+        title: 'Error',
+        message: fail?.custom_fail_message || e.first_message || e.message,
+        type: 'error'
+      })
+    }
+    if (fail?.callback) fail?.callback()
     throw e
   } finally {
-    // loadingStore.setTableLoading(false)
-    // loadingStore.setfullscreenLoading(false)
+    fullscreenLoading?.close()
   }
 }
